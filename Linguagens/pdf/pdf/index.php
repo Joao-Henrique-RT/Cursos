acf813e5314dd51fb56a93fd2026d1822ea9a7b6
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2023-04-26 20:26:23 -0300

    criação testes de php PDF

---
+++ Linguagens/pdf/pdf/index.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste Pdf</title>
+</head>
+<body>
+    
+    <h1>teste</h1>
+
+    <a href="gerar_pdf.php">Gerar Relatório</a>
+
+
+
+
+
+</body>
+</html>
