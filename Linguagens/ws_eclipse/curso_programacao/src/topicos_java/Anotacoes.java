e1c7a10d650091e44c860781342d61e71601725e
Joaohrtmcpe <joaohenriquert@outlook.com>
2023-04-29 19:35:19 -0300

    att

---
+++ Linguagens/ws_eclipse/curso_programacao/src/topicos_java/Anotacoes.java
@@ -0,0 +1,27 @@
+package topicos_java;
+
+public class Anotacoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Classe para anotações.
+		
+		/*Padrão camelCase é utilizado para pacotes, variaveis, metodos, etc.
+		 * 
+		 * Para classes utilizamso o padrão Pascalcase, onde a primeira letra é maiúscula
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		
+	}
+
+}
