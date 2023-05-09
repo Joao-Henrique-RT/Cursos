94b0fce28cd805ec84ed590001d9a8681578452b
Joaohrtmcpe <joaohenriquert@outlook.com>
2023-05-09 00:02:48 -0300

    att

---
+++ Linguagens/ws_eclipse/curso_programacao/src/entities/Square.java
@@ -0,0 +1,11 @@
+package entities;
+
+/*Foi criada a classe para representar o quadrado, neste caso vamos 
+	adicionar atributos (a,b) ao nosso quadrado.
+*/
+public class Square {
+		public double a;
+		public double b;
+	
+
+}
--- Linguagens/ws_eclipse/curso_programacao/src/entities/Triangle.java
+++ Linguagens/ws_eclipse/curso_programacao/src/entities/Triangle.java
@@ -1,8 +1,26 @@
 package entities;
+/*Classe publica triangle, dentro da claase declaramos os membros
+da classe, adicionamos os atributos a,b e c. 
 
+- São public e por isso podem ser acessadas por outros arquivos.
+
+- Criamos dentro do pacote de entidades poruqi eo trinagulo é uma entidade do nosso 
+Projeto/programa 
+
+- Para pegarmos estes parametros precisamos instanciar a classe em
+nosso programa
+
+ex; Triangle x, y;
+x = new Triangle();
+y = new Triangle ();
+
+
+*/
 public class Triangle {
-		public double a;
-		public double b;
-		public double c;
+		public double a; //Atributo!
+		public double b; //atributo!
+		public double c; //atributo!
 		
 }
+
+
