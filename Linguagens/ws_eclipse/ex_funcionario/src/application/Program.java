a16a95baeabd805b213da5e413915861245af159
Joaohrtmcpe <joaohenriquert@outlook.com>
2023-05-24 16:40:08 -0300

    .

--- Linguagens/ws_eclipse/ex_funcionario/src/application/Program.java
+++ Linguagens/ws_eclipse/ex_funcionario/src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Employee;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -10,9 +12,25 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		
+		Employee employee = new Employee();
+		
 		Scanner leia = new Scanner(System.in);
 		
+		System.out.println("Digite seu nome: ");
+		employee.name = leia.next();
 		
+		System.out.println("Digite seu salário bruto: ");
+		employee.grossSalary = leia.nextDouble();
+		
+		System.out.println("Digite a taxa de desconto: ");
+		employee.tax = leia.nextDouble();
+		
+		System.out.println(employee.toString());
+		
+		System.out.println("Which percentage to increase salary?");	
+		double percentage = leia.nextDouble();
+		
+		System.out.println("updated data: " + employee.name + ",  $ " + employee.increaseSalary(percentage));
 		
 		
 	}
--- Linguagens/ws_eclipse/ex_funcionario/src/entities/Employee.java
+++ Linguagens/ws_eclipse/ex_funcionario/src/entities/Employee.java
@@ -1,17 +1,25 @@
 package entities;
 
-public class Employee {
+public class Employee { //funcionário
 
 	public String name;
 	public double grossSalary;//Salário bruto
 	public double tax; //taxa
 	
 	public double netSalary() { //salario liquido
+		return grossSalary - tax;
 		
 	}
 	
-	public void increaseSalary(double percentage) { //percentage  será atribuida no programa
-		
+	public double increaseSalary(double percentage) { //percentage  será atribuida no programa
+		return grossSalary += (grossSalary * percentage)/100;
+	}
+	
+	public String toString() {
+		return "Employee: " +
+				name + 
+				",  $ " +
+				String.format("%.2f", netSalary());
 	}
 	
 	
