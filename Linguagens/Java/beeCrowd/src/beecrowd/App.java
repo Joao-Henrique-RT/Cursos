d2e8c94e5cbdbbb36a9be307ebbd013377de7a68
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2023-11-11 15:56:24 -0300

    java

---
+++ Linguagens/Java/beeCrowd/src/beecrowd/App.java
@@ -0,0 +1,20 @@
+package beecrowd;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner leia = new Scanner(System.in);
+        
+         int a,b,x;
+
+        a = leia.nextInt();
+        b = leia.nextInt();
+
+        x = (a+b);
+
+
+         
+        System.out.println(x);
+    }
+}
