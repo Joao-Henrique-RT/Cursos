729519137521421ce7107c2e78ac1e27c28f4487
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2025-02-11 08:00:56 -0300

    Unidade 2

--- CIencia_Computacao/Programacao_Computadores/app.py
+++ CIencia_Computacao/Programacao_Computadores/app.py
@@ -1,13 +1,25 @@
-nota1 = int(input("Digite a primeira nota"))
-nota2 = int(input("Digite a segunda nota"))
-frequencia = int(input("Digite a frequencia"))
+# nota1 = int(input("Digite a primeira nota"))
+# nota2 = int(input("Digite a segunda nota"))
+# frequencia = int(input("Digite a frequencia"))
 
-if(frequencia >= 75):
-    soma = (nota1 + nota2)  
-    if(soma>6):
-        print("ALuno aprovado com nota:", soma)
+# if(frequencia >= 75):
+#     soma = (nota1 + nota2)  
+#     if(soma>6):
+#         print("ALuno aprovado com nota:", soma)
         
-    elif(soma>2 and soma<6):        
-        print("Aluno irá para recuperação")
-else:
-    print("Aluno reprovado")
+#     elif(soma>2 and soma<6):        
+#         print("Aluno irá para recuperação")
+# else:
+#     print("Aluno reprovado")
+
+#Verificando se número é par ou impar
+
+# num = int(input("DIgite um número"))
+
+# if(num % 2 == 0):
+#     print("Número é par")
+
+# else:
+#     print("Número é impar")
+
+
