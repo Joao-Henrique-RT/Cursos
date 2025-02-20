1e4e331f5de9a772c69b5195baf3b2ce3897a0b0
João Henrique Ribeiro Teixeira <86114318+Joao-Henrique-RT@users.noreply.github.com>
2025-02-20 08:27:51 -0300

    UNIDADES 3 E 4

--- Linguagens/Udemy/Python/ex_lista1/tabuada.py
+++ Linguagens/Udemy/Python/ex_lista1/tabuada.py
@@ -1,6 +1,32 @@
-num1 = int(input("Digite o numero que irá multiplicar"))
-num2 = int(input("Digite quatras vezes o numero irá multiplicar"))
+# num1 = int(input("Digite o numero que irá multiplicar"))
+# num2 = int(input("Digite quatras vezes o numero irá multiplicar"))
 
-for i in range(num2+1): #PARA SER EXIBIDO ATÉ O NÚMERO QUE FOI INFORMADO
-   print(str(num1) + "X" + str(i) + "=" + str(i*num1))
+# for i in range(num2+1): #PARA SER EXIBIDO ATÉ O NÚMERO QUE FOI INFORMADO
+#    print(str(num1) + "X" + str(i) + "=" + str(i*num1))
 
+
+
+# sexo = ""
+# idade = 0
+# qtdMulheres = 0
+# qtdHomens = 0
+# valorIdade = 0
+
+# for i in range(0,3):
+#    sexo = input("Digite o sexo M ou H")
+#    idade = int(input("Digite a idade"))
+
+#    if (sexo=="M" or "m"):
+#       qtdMulheres+=1
+
+#       valorIdade+=idade
+
+# mediaIdade = valorIdade/qtdMulheres
+
+# print("MUlheres: ",qtdMulheres)
+# print("Media ", mediaIdade)
+
+
+dicionario = {"Nome": "João", "Idade": "21", "Email": "joaolindo"}
+dicionario.popitem("Idade")
+print(dicionario)
